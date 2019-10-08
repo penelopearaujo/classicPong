d61ce5a96e1dc3757f6be1f4da1fdd9cc874810a
Penélope Araújo <pmpa@cin.ufpe.br>
2019-10-07 23:37:07 -0300

    implementation of player 2

--- pong.js
+++ pong.js
@@ -1,25 +1,29 @@
-// variáveis da bola
-let xBall = 300
-let yBall = 200
-let diamBall = 20
-let raio = diamBall/2;
+//variáveis da bolinha
+let xBall = 300;
+let yBall = 200;
+let diametro = 15;
+let raio = diametro / 2 ;
 
-// velocidade da bola
-let velocityXBall = 3
-let velocityYBall = 3
-
-//variáveis da raquete
-let xRaquete = 30;
-let yRaquete = 150;
+//velocidade da bolinha
+let velocidadexBall = 6;
+let velocidadeyBall = 6;
 let raqueteComprimento = 10;
 let raqueteAltura = 90;
 
-let colidiu = false
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
 
 //variáveis do oponente
-let xRaqueteOponente = 560;
+let xRaqueteOponente = 585;
 let yRaqueteOponente = 150;
-let velocityYOponente;
+let velocidadeYOponente;
+
+let colidiu = false;
+
+//placar do jogo
+let meusPontos = 0;
+let pontosDoOponente = 0;
 
 function setup() {
   createCanvas(600, 400);
@@ -27,58 +31,86 @@ function setup() {
 
 function draw() {
   background(60);
-  showBall();
-  moveBall();
-  showRaquete(xRaquete, yRaquete);
-  showRaquete(xRaqueteOponente, yRaqueteOponente);
-  moveRaquete();
-  checkCollisionLib();
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete(xRaquete, yRaquete);
+  movimentaMinhaRaquete();
+  //verificaColisaoRaquete();
+  verificaColisaoRaquete(xRaquete, yRaquete);
+  mostraRaquete(xRaqueteOponente, yRaqueteOponente);
+  movimentaRaqueteOponente();
+  verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);
+  incluiPlacar();
+  marcaPonto();
 }
 
-function showRaquete(x, y) {
-  rect(x, y, raqueteComprimento, raqueteAltura);
+function mostraBolinha(){
+  circle(xBall, yBall, diametro);
 }
 
-function moveRaquete() {
-    if (keyIsDown(UP_ARROW)) {
-        yRaquete -= 10;
-    }
-    if (keyIsDown(DOWN_ARROW)) {
-        yRaquete += 10;
-    }
+function movimentaBolinha(){
+  xBall += velocidadexBall;
+  yBall += velocidadeyBall;
 }
 
-function moveRaqueteOponente() {
-  velocityYOponente = yBall - yRaqueteOponente - raqueteComprimento / 2 - 30;
-    yRaqueteOponente += velocidadeYOponente
-}
-
-function showBall() {
-  circle(xBall, yBall, diamBall);
-}
-
-function moveBall() {
-  xBall += velocityXBall
-  yBall += velocityYBall
-  
-  // raio da bolinha não sai da tela
-  if (xBall+raio > width || xBall-raio < 0) {
-    velocityXBall *= -1;
+function verificaColisaoBorda(){
+  if (xBall + raio> width ||
+     xBall - raio< 0){
+    velocidadexBall *= -1;
   }
-  
-  if (yBall+raio > height || yBall-raio < 0) {
-    velocityYBall *= -1;
+  if (yBall + raio> height ||
+     yBall - raio < 0){
+    velocidadeyBall *= -1;
   }
 }
 
-// function checkCollision() {
-//     if (xBall - raio < xRaquete + raqueteComprimento && yBall - raio < yRaquete + raqueteAltura && yBall + raio > yRaquete) {
-//         velocityXBall *= -1;
-//     }
-// }
+function mostraRaquete(x,y){
+  rect(x, y, raqueteComprimento, 
+      raqueteAltura);
+}
 
-function checkCollisionLib() {
-  colidiu = collideRectCircle(xRaquete, yRaquete, raqueteComprimento, raqueteAltura, xBall, yBall, raio);
-  if (colidiu) {
-    velocityXBall *= -1;
-  }}
+function movimentaMinhaRaquete(){
+  if (keyIsDown(UP_ARROW)){
+    yRaquete -= 10;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    yRaquete += 10;
+  }
+}
+
+function verificaColisaoRaquete(){
+  if (xBall - raio < xRaquete + raqueteComprimento && 
+      yBall - raio < yRaquete + raqueteAltura && 
+      yBall + raio > yRaquete){
+    velocidadexBall *= -1;
+  }
+}
+
+function verificaColisaoRaquete(x, y){
+  colidiu = collideRectCircle(x, y,raqueteComprimento,raqueteAltura,
+                              xBall,yBall,raio);
+  if (colidiu){
+    velocidadexBall *= -1;
+  }
+}
+
+function movimentaRaqueteOponente(){
+  velocidadeYOponente = yBall - yRaqueteOponente - raqueteComprimento / 2 - 30;
+  yRaqueteOponente += velocidadeYOponente
+}
+
+function incluiPlacar(){
+  fill(255);
+  text(meusPontos, 278, 26);
+  text(pontosDoOponente, 321, 26)
+}
+
+function marcaPonto(){
+  if (xBall > 590){
+    meusPontos += 1;
+  }
+  if (xBall < 10){
+    pontosDoOponente += 1;
+  }
+}
